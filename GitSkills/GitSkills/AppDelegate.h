8d9256ecc6b28de48c0eb8b534658c280aafe078
王嘉宁 <wangjianing@wangjianingdeMacBook-Pro.local>
2018-12-08 16:28:58 +0800

    add project

---
+++ GitSkills/GitSkills/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitSkills
+//
+//  Created by 王嘉宁 on 2018/12/8.
+//  Copyright © 2018 Johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitSkills/GitSkills/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GitSkills
+//
+//  Created by 王嘉宁 on 2018/12/8.
+//  Copyright © 2018 Johnny. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
